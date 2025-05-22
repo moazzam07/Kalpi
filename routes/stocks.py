de301542f075f8ad2ef7deab9654a69ef9f2f9e4
moazzam khan <kmohdmoazzam70@gmail.com>
2025-05-22 17:59:10 +0530

    Made Api's uniformed

--- routes/stocks.py
+++ routes/stocks.py
@@ -3,11 +3,11 @@ import schemas, utils
 
 router = APIRouter()
 
+@router.get("/")
+async def list_stocks(limit: int = 100):
+    return await utils.get_stocks(limit=limit)
+
 @router.post("/create")
 async def create_stock(stock: schemas.Stock):
     stock_data = stock.dict(by_alias=True)
     return await utils.create_stock(stock_data)
-
-@router.get("/")
-async def list_stocks(limit: int = 100):
-    return await utils.get_stocks(limit=limit)
