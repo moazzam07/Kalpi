b071985cacbef62ab01986724273d5fe26b9ee7f
moazzam khan <kmohdmoazzam70@gmail.com>
2025-05-22 17:31:40 +0530

    created routes for screen and stocks

---
+++ routes/stocks.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+import schemas, utils
+
+router = APIRouter()
+
+@router.post("/")
+async def create_stock(stock: schemas.Stock):
+    stock_data = stock.dict(by_alias=True)
+    return await utils.create_stock(stock_data)
+
+@router.get("/")
+async def list_stocks(limit: int = 100):
+    return await utils.get_stocks(limit=limit)
